bf74f76ce70d7eb35cb303ae4fff2b42d546ce97
langamitch <langamitch69@gmail.com>
2026-03-28 19:05:16 +0200

    changed foreground color

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ const Page = () => {
         new Date().toLocaleTimeString('en-US', {
           hour: '2-digit',
           minute: '2-digit',
-          hour12: false,
+          hour12: true,
         })
       )
     }
