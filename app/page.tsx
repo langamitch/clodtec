dcd04475fa9e0b0e286677a0c5281e11f7d4b4f8
langamitch <langamitch69@gmail.com>
2026-03-28 19:23:31 +0200

    monor fixes

--- app/page.tsx
+++ app/page.tsx
@@ -33,6 +33,7 @@ const Page = () => {
         new Date().toLocaleTimeString('en-US', {
           hour: '2-digit',
           minute: '2-digit',
+          second: '2-digit',
           hour12: true,
         })
       )
